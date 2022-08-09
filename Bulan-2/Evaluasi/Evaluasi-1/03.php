60c6e399e23ae4c8daa9772ee7101f34e10b97ee
rizzturu13 <hasanrizky0405@gmail.com>
2022-08-09 10:07:05 +0700

    Tugas

--- Bulan-2/Evaluasi/Evaluasi-1/03.php
+++ Bulan-2/Evaluasi/Evaluasi-1/03.php
@@ -1,43 +1,35 @@
 <?php
-echo "Nama : ";
-$name =trim(fgets(STDIN));
-echo "NIK : ";
-$nik =trim(fgets(STDIN));
-echo "Jurusan : ";
-$jurusan =trim(fgets(STDIN));
-echo "Divisi : ";
-$divisi =trim(fgets(STDIN));
-echo "Umur : ";
-$age =trim(fgets(STDIN));
+echo "Data yang ingin di inputkan : ";
+$data =trim(fgets(STDIN));
+for($x=0; $x<$data; $x++){
+    $student = [];
+    echo "Nama : ";
+    $name =trim(fgets(STDIN));
+    echo "NIK : ";
+    $nik =trim(fgets(STDIN));
+    echo "Jurusan : ";
+    $jurusan =trim(fgets(STDIN));
+    echo "Divisi : ";
+    $divisi =trim(fgets(STDIN));
+    echo "Umur : ";
+    $age =trim(fgets(STDIN));
 
-$students = [
-    [
-        'name' => $name,
-        'nik'  => $nik,
-        'jurusan' => $jurusan,
-        'divisi' => $divisi,
-        'age' => $age
-    ],
-    [
-        'name' => $name,
-        'nik' => $nik,
-        'jurusan' => $jurusan,
-        'divisi' => $divisi,
-        'age' => $age
-    ]
-  ];
+    $students = [
+        [
+            'name' => $name,
+            'nik'  => $nik,
+            'jurusan' => $jurusan,
+            'divisi' => $divisi,
+            'age' => $age
+        ],
+        [
+            'name' => $name,
+            'nik' => $nik,
+            'jurusan' => $jurusan,
+            'divisi' => $divisi,
+            'age' => $age
+        ]
+    ];
 
-$array_name= [];
-$array_nik= [];
-$array_jurusan= [];
-$array_divisi= [];
-$array_age= [];
-
-foreach($student as $key => $value){
-    $array_name[$key] = $value['name'];
-    $array_nik[$key] = $value['nik'];
-    $array_jurusan[$key] = $value['jurusan'];
-    $array_divisi[$key] = $value['divisi'];
-    $array_age[$key] = $value['age'];
 }
-print_r($array_jurusan);
+print_r($students);
